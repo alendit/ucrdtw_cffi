ed4292a983c4ce79309fa0e6a423b216d0d7e0eb
Dimitri Vorona <vorona@in.tum.de>
2017-09-18 13:31:22 +0200

    Update tests

--- test/test_rw.py
+++ test/test_rw.py
@@ -11,7 +11,8 @@ def test_rw():
     query = np.cumsum(np.random.uniform(-0.5, 0.5, 100)).astype(np.float)
     loc, dist = dtw_query(query, data, 0.05)
 
-    print(loc, dist)
+    assert loc > 0
+    assert dist > 0
 
     # query = np.concatenate((np.linspace(0.0, 0.0, loc), query)) + (data[loc] - query[0])
     # plt.figure()
@@ -23,28 +24,33 @@ def test_sin():
     data = np.cos(np.linspace(0.0, math.pi * 6, 600)) + (np.random.uniform(-0.5, 0.5, 600) / 10)
     query = np.sin(np.linspace(0.0, math.pi * 2, 200))
 
-    plt.figure()
-    plt.plot(data)
-    plt.plot(query)
+
     loc, dist = dtw_query(query, data, 0.05)
-    print(loc, dist)
-    matched = np.concatenate((np.linspace(0.0, 0.0, loc), query))
-    plt.plot(matched)
-    plt.show()
+
+    assert loc > 0
+    assert dist > 0
+
+    # matched = np.concatenate((np.linspace(0.0, 0.0, loc), query))
+    # plt.figure()
+    # plt.plot(data)
+    # plt.plot(query)
+    # plt.plot(matched)
+    # plt.show()
 
 def test_static_data():
     data = np.fromfile(os.path.join(os.path.dirname(__file__), 'data/data.txt'), dtype="double", sep="\n")
     query = np.fromfile(os.path.join(os.path.dirname(__file__), 'data/query.txt'), dtype="double", sep="\n")
 
     loc, dist = dtw_query(query, data, 0.05)
-    print(loc, dist)
+    assert loc > 0
+    assert dist > 0
 
-    plt.figure()
-    plt.plot(data)
-    plt.plot(query)
-    matched = np.concatenate((np.linspace(0.0, 0.0, loc), query))
-    plt.plot(matched)
-    plt.show()
+    # plt.figure()
+    # plt.plot(data)
+    # plt.plot(query)
+    # matched = np.concatenate((np.linspace(0.0, 0.0, loc), query))
+    # plt.plot(matched)
+    # plt.show()
 
 if __name__=="__main__":
     test_rw()
